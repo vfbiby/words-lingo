cd7ffa198068825852192e812f653e9218f6d5b5
Musk Zhang <muskzhang2024@gmail.com>
2025-01-12 23:52:20 +0800

    Add migration for users table with necessary fields and constraints

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20250107_193112_create_words_table;
+mod m20250107_193113_create_users_table;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20250107_193112_create_words_table::Migration),
+            Box::new(m20250107_193113_create_users_table::Migration),
         ]
     }
 }
---
+++ migration/src/m20250107_193113_create_users_table.rs
@@ -0,0 +1,43 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(pk_auto(User::UserId))
+                    .col(string(User::Username).not_null())
+                    .col(string(User::Email).unique_key().not_null())
+                    .col(string(User::PasswordHash).not_null())
+                    .col(
+                        ColumnDef::new(User::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    UserId,
+    Username,
+    Email,
+    PasswordHash,
+    CreatedAt,
+}
